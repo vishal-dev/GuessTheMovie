8df43287f104102ed885dd4ce41795d96cdb38c0
vishal <vishalkumarp@gmail.com>
2018-03-30 15:01:30 -0400

    Adding test

---
+++ tests/GuessMovie/GameTest.java
@@ -0,0 +1,18 @@
+package GuessMovie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+    @Test
+    public void readMovies() throws Exception {
+    }
+
+    @Test
+    public void selectMovie() throws Exception {
+        String testingName = "Name test";
+        assertEquals("Name test",testingName);
+    }
+
+}
